0917458909cc08e6a2b80fa5f7116f44ae2ca02d
cheshireoctopus <chandlermoisen@gmail.com>
2014-01-08 13:23:16 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babo::Application.config.secret_key_base = 'f0134a524c1a29c5c9c580525b9d1eaf073c9bac8bde19ed0f81df65ed47b58bfd602df510f8ce1755b1d1f0be9266c659d836cedd74d7f46118aa048af6766f'
