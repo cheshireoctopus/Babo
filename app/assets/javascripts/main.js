d14a8b03607cce32db88ae53f04149f7829fe54c
cheshireoctopus <chandlermoisen@gmail.com>
2014-01-13 17:08:22 -0500

    Country name display on hover

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -28,41 +28,39 @@ function map() {
 
   // Ready function; called from 'queue'; takes three params: error, world (1st defer), names (2nd defer);
   function ready(error, world, names) {
-    console.log("error:");
-    console.log(error);
-    console.log("world:");
-    console.log(world);
-    console.log("names:");
-    console.log(names);
+    // console.log("error:");
+    // console.log(error);
+    // console.log("world:");
+    // console.log(world);
+    // console.log("names:");
+    // console.log(names);
     // ?
     var countries = topojson.feature(world, world.objects.countries).features;
-    console.log("world.objects.countries:");
-    console.log(world.objects.countries);
-    console.log("countries:");
-    console.log(countries);
+    // console.log("world.objects.countries:");
+    // console.log(world.objects.countries);
+    // console.log("countries:");
+    // console.log(countries);
 
     //Matches country projection with name from tsv
-    console.log("var d:");
     countries.forEach(function(d) {
-      console.log(d);
       d.name = names.filter(function(n) { return d.id == n.id; })[0].name;
     });
 
     // ?
     var country = svg.selectAll(".mmcountry").data(countries);
-    console.log("var country:");
-    console.log(country);
 
     // ?
     country.enter().insert("path").attr("class", function(d, i) {
-      console.log(d);
-      console.log(i);
       return hoverSpecific(d, i);
     }).attr("title", function(d, i) {
       return d.name;
     }).attr("d", path)
     .on("mouseover", function(d, i){
-      console.log(d.name);
+      d3.select("#mapinfo").style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY) + "px");
+      d3.select("#mapinfo").html("<p>"+d.name+"<p>").classed("hidden", false);
+    })
+    .on("mouseout", function(){
+      d3.select("#mapinfo").classed("hidden", true);
     });
 
 
