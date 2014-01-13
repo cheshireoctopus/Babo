307fd451b79a58215fa055931bf336e7eac6411e
cheshireoctopus <chandlermoisen@gmail.com>
2014-01-13 16:53:32 -0500

    D3 country hover

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,35 +20,57 @@ function map() {
   // var tt_Kenya = d3.select("#map").append("div").attr("class", "mminfo").attr("id", "tt_Kenya");
   // tt_Kenya.attr("style", "left:472px;top:275px").html("<div class='mmtitle'>KENYA</div>");
 
-  // Queue's appropriate json and tsv files; awaits function 'ready'
+  // Queue's appropriate json and tsv files; awaits function 'ready'; the order of files deferred matters
   queue()
     .defer(d3.json, "d3-world.json")
     .defer(d3.tsv, "names.tsv")
     .await(ready);
 
-  // Ready function; called from 'queue'
+  // Ready function; called from 'queue'; takes three params: error, world (1st defer), names (2nd defer);
   function ready(error, world, names) {
-
+    console.log("error:");
+    console.log(error);
+    console.log("world:");
+    console.log(world);
+    console.log("names:");
+    console.log(names);
+    // ?
     var countries = topojson.feature(world, world.objects.countries).features;
-    console.log(countries[0]);
+    console.log("world.objects.countries:");
+    console.log(world.objects.countries);
+    console.log("countries:");
     console.log(countries);
 
     //Matches country projection with name from tsv
+    console.log("var d:");
     countries.forEach(function(d) {
+      console.log(d);
       d.name = names.filter(function(n) { return d.id == n.id; })[0].name;
     });
 
+    // ?
     var country = svg.selectAll(".mmcountry").data(countries);
+    console.log("var country:");
+    console.log(country);
 
+    // ?
     country.enter().insert("path").attr("class", function(d, i) {
+      console.log(d);
+      console.log(i);
       return hoverSpecific(d, i);
     }).attr("title", function(d, i) {
       return d.name;
-    }).attr("d", path);
+    }).attr("d", path)
+    .on("mouseover", function(d, i){
+      console.log(d.name);
+    });
+
+
 
   }
   //End 'Ready' function
 
+  // CSS Hover function; called from with in 'ready' function
   function hoverSpecific(d, i) {
     return 'mmcountry countryHover';
   }
