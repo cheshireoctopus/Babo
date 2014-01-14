d6a9c91386cca041be50ca6cede9328293e979a9
cheshireoctopus <chandlermoisen@gmail.com>
2014-01-14 18:46:28 -0500

    d3 styling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
 $(function() {
   map();
-  color = d3.scale.linear().domain([15,89]).range(["rgb(192,192,192)","rgb(42,95,62)"]);
+  color = d3.scale.linear().domain([5,89]).range(["rgb(192,192,192)","rgb(42,95,62)"]);
 });
 
 function map() {
@@ -17,10 +17,6 @@ function map() {
   // Defines svg
   var svg = d3.select("#map").append("svg").attr("width", width).attr("height", height);
 
-  // Define info div
-  // var tt_Kenya = d3.select("#map").append("div").attr("class", "mminfo").attr("id", "tt_Kenya");
-  // tt_Kenya.attr("style", "left:472px;top:275px").html("<div class='mmtitle'>KENYA</div>");
-
   // Queue's appropriate json and tsv files; awaits function 'ready'; the order of files deferred matters
   queue()
     .defer(d3.json, "d3-world.json")
@@ -29,18 +25,9 @@ function map() {
 
   // Ready function; called from 'queue'; takes three params: error, world (1st defer), names (2nd defer);
   function ready(error, world, names) {
-    // console.log("error:");
-    // console.log(error);
-    // console.log("world:");
-    // console.log(world);
-    // console.log("names:");
-    // console.log(names);
+
     // ?
     var countries = topojson.feature(world, world.objects.countries).features;
-    // console.log("world.objects.countries:");
-    // console.log(world.objects.countries);
-    // console.log("countries:");
-    // console.log(countries);
 
     //Matches country projection with name and number from tsv
     countries.forEach(function(d) {
@@ -48,34 +35,29 @@ function map() {
       d.number = names.filter(function(n) { return d.id == n.id; })[0].number;
     });
 
-    // ?
-    var country = svg.selectAll(".mmcountry").data(countries);
+    // Define var 'country' as all svg elements with class 'country'; append data from var 'countries'
+    var country = svg.selectAll(".country").data(countries);
 
     // ?
-    country.enter().insert("path").attr("class", function(d, i) {
-      return hoverSpecific(d, i);
-    }).attr("title", function(d, i) {
-      return d.name;
-    }).attr("d", path)
-    .on("mouseover", function(d, i){
-      d3.select("#mapinfo").style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY) + "px");
-      d3.select("#mapinfo").html("<p>"+d.name+"</p>"+"<p>"+d.number+"</p>").classed("hidden", false);
-    })
-    .on("mouseout", function(){
-      d3.select("#mapinfo").classed("hidden", true);
-    })
-    .style("fill", function(d){
-      return color(d.number);
-    });
-
-
-
+    country.enter().insert("path")
+      .attr("class", function(d, i){
+        return "country";
+      })
+      .attr("title", function(d, i) {
+        return d.name;
+      }).attr("d", path)
+      .on("mouseover", function(d, i){
+        d3.select("#mapinfo").style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY) + "px");
+        d3.select("#mapinfo").html("<p>"+d.name+"</p>"+"<p>"+d.number+"</p>").classed("hidden", false);
+      })
+      .on("mouseout", function(){
+        d3.select("#mapinfo").classed("hidden", true);
+      })
+      .style("fill", function(d){
+        return color(d.number);
+      })
+      .style("stroke", "black")
+      .style("stroke-width", "0.25px");
   }
   //End 'Ready' function
-
-  // CSS Hover function; called from with in 'ready' function
-  function hoverSpecific(d, i) {
-    return 'mmcountry countryHover';
-  }
-
 }
