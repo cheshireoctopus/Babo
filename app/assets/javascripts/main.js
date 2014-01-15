fa69bfcf10610f65b3590c3945eea31be56e4f7d
cheshireoctopus <chandlermoisen@gmail.com>
2014-01-14 20:10:50 -0500

    d3 work

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,21 +1,24 @@
 $(function() {
+  w = ($('#map').width());
+  h = (w/2.5);
   map();
-  color = d3.scale.linear().domain([5,89]).range(["rgb(192,192,192)","rgb(42,95,62)"]);
 });
 
 function map() {
+  color = d3.scale.linear().domain([5,89]).range(["rgb(192,192,192)","rgb(42,95,62)"]);
+
   // The SVG container for map
   var width = 960,
   height = 430;
 
   // Defines projection as an equirectangular projection, translates, and scales it
-  var projection = d3.geo.equirectangular().translate([470, 260]).scale(155);
+  var projection = d3.geo.equirectangular().translate([w/2.05, 260]).scale(175);
 
   // Defines the path of the projection
   var path = d3.geo.path().projection(projection);
 
   // Defines svg
-  var svg = d3.select("#map").append("svg").attr("width", width).attr("height", height);
+  var svg = d3.select("#map").append("svg").attr("width", w).attr("height", h).attr("id", "world");
 
   // Queue's appropriate json and tsv files; awaits function 'ready'; the order of files deferred matters
   queue()
@@ -47,17 +50,27 @@ function map() {
         return d.name;
       }).attr("d", path)
       .on("mouseover", function(d, i){
+        d3.select(this).style("stroke","red").style("stroke-width","2px");
         d3.select("#mapinfo").style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY) + "px");
         d3.select("#mapinfo").html("<p>"+d.name+"</p>"+"<p>"+d.number+"</p>").classed("hidden", false);
       })
       .on("mouseout", function(){
+        d3.select(this).style("stroke","black").style("stroke-width","0.25px");
         d3.select("#mapinfo").classed("hidden", true);
       })
       .style("fill", function(d){
         return color(d.number);
       })
-      .style("stroke", "black")
+      .style("stroke","black")
       .style("stroke-width", "0.25px");
   }
   //End 'Ready' function
+
+}
+
+
+function hideAnt() {
+  world = $('#world');
+  abc = world.children()[6];
+  abc.style.display='none';
 }
